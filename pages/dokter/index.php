835fbb7403a4e9012daa826d373ee485959cde96
cindyesa <91884180+cindyesa@users.noreply.github.com>
2024-12-13 11:48:31 +0700

    Update index.php

--- pages/dokter/index.php
+++ pages/dokter/index.php
@@ -30,26 +30,26 @@ $totalPatients = mysqli_fetch_assoc($totalPatientsResult)['total'];
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Poliklinik | Dashboard</title>
 
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+   <!-- Google Font: Source Sans Pro -->
+   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST'] ?>/bk_poliklinik/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST'] ?>/cindy/plugins/fontawesome-free/css/all.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Tempusdominus Bootstrap 4 -->
-  <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST'] ?>/bk_poliklinik/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST'] ?>/cindy/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
   <!-- iCheck -->
-  <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST'] ?>/bk_poliklinik/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST'] ?>/cindy/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
   <!-- JQVMap -->
-  <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST'] ?>/bk_poliklinik/plugins/jqvmap/jqvmap.min.css">
+  <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST'] ?>/cindy/plugins/jqvmap/jqvmap.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST'] ?>/bk_poliklinik/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST'] ?>/cindy/dist/css/adminlte.min.css">
   <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST'] ?>/bk_poliklinik/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST'] ?>/cindy/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
   <!-- Daterange picker -->
-  <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST'] ?>/bk_poliklinik/plugins/daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST'] ?>/cindy/plugins/daterangepicker/daterangepicker.css">
   <!-- summernote -->
-  <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST'] ?>/bk_poliklinik/plugins/summernote/summernote-bs4.min.css">
+  <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST'] ?>/cindy/plugins/summernote/summernote-bs4.min.css">
 </head>
 
 <style>
